c3d2b7d12c608af70fc5479c51feafa11136390f
Sofia <sofia.rebechi@gmail.com>
2022-03-30 19:14:40 -0300

    primer punto, hola mundo

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-main() {
-
+int main() {
+	
+	//punto 3a
 	printf("Hola mundo\n");
+
+	//punto 3e
+
+	int variable;
+	int *puntero = &variable;
+
+	printf("Contenido del puntero : %i \n", puntero);
+
+	return 0;
+
 }
