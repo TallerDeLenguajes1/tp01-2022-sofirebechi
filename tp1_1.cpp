6a787c0426f5d7c7149e56291e805ca0328e940f
Sofia <sofia.rebechi@gmail.com>
2022-03-30 21:01:10 -0300

    segundo commit, punto 3 terminado

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -4,15 +4,19 @@
 int main() {
 	
 	//punto 3a
+	
 	printf("Hola mundo\n");
 
 	//punto 3e
-
 	int variable;
-	int *puntero = &variable;
+	int *puntero;
+	puntero = &variable;
 
-	printf("Contenido del puntero : %i \n", puntero);
-
+	printf("Contenido del puntero : %d \n", puntero);
+	printf("Direccion de memoria almacenada por el puntero: %d \n", &variable );
+	printf("Direccion de memoria de la variable: %d \n", &variable);
+	printf("Direccion de memoria del puntero: %d \n ", &puntero);
+	printf("Tamaño de direccion de memoria utilizado por la variable: %d \n", sizeof(&variable));
 	return 0;
 
 }
